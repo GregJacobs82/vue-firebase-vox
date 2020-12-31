c47a65b2f6523db69a220a2a67326a99fc21dd21
Greg <Greg@2book.com>
2020-12-31 15:34:53 -0500

    Auth: Login Anonymously

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,17 @@
 <template>
-    <div>
+    <div class="container">
         <h1>Homepage</h1>
+
+        <LoginAnonymously />
     </div>
 </template>
+
+<script>
+    import LoginAnonymously from "./LoginAnonymously";
+
+    export default {
+        components: {
+            LoginAnonymously,
+        }
+    }
+</script>
